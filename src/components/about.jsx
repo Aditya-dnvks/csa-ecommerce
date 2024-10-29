669824228904ff70c0d061159caf3c6b078ccac2
Aditya-dnvks <aditya.dnvks@gmail.com>
2024-10-29 18:46:00 +0530

    Login feature by Karimulla

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -3,7 +3,7 @@ import "./about.css";
 const About = () => {
   return (
     <div>
-      <h1 className="submit-button1"> This is About Page</h1>
+      <h1 className="submit-button1"> This is About Page. Welcome</h1>
     </div>
   );
 };
