417feeca8038a12db02dababf327f59a0bcd4fbb
deekondarushikesh2299 <deekondarushikesh2299@gmail.com>
2024-12-04 18:27:06 +0530

    design login signup

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,50 +1,18 @@
+import { Button, Heading, TextField } from "@radix-ui/themes";
 import "./login.css";
-import * as Form from "@radix-ui/react-form";
+
 const Login = () => {
   return (
     <div className="d-flex flex-row justify-content-around align-items-center bg-container">
-      <img src="https://img.freepik.com/free-vector/mobile-login-concept-illustration_114360-83.jpg" />
-      <Form.Root className="FormRoot">
-        <Form.Field className="FormField" name="email">
-          <div
-            style={{
-              display: "flex",
-              alignItems: "baseline",
-              justifyContent: "space-between",
-            }}
-          >
-            <Form.Label className="FormLabel">Email</Form.Label>
-            <Form.Message className="FormMessage" match="valueMissing">
-              Please enter your email
-            </Form.Message>
-          </div>
-          <Form.Control asChild>
-            <input className="Input" type="email" required />
-          </Form.Control>
-        </Form.Field>
-        <Form.Field className="FormField" name="password">
-          <div
-            style={{
-              display: "flex",
-              alignItems: "baseline",
-              justifyContent: "space-between",
-            }}
-          >
-            <Form.Label className="FormLabel">Password</Form.Label>
-            <Form.Message className="FormMessage" match="valueMissing">
-              Enter the password
-            </Form.Message>
-          </div>
-          <Form.Control asChild>
-            <input className="Input" type="password" required />
-          </Form.Control>
-        </Form.Field>
-        <Form.Submit asChild>
-          <button className="Button" style={{ marginTop: 10 }}>
-            Login
-          </button>
-        </Form.Submit>
-      </Form.Root>
+      <img src="https://img.freepik.com/free-vector/tablet-login-concept-illustration_114360-7963.jpg?semt=ais_hybrid" />
+      <form className="FormRoot">
+        <Heading className="text-center">Login Form</Heading>
+        <TextField.Root  required placeholder="Email" className="m-4"></TextField.Root>
+        <TextField.Root   required  placeholder="Password" className="m-4"></TextField.Root>
+        <div className="text-center">
+          <Button>Login</Button>
+        </div>
+      </form>
     </div>
   );
 };
--- src/components/signup/signup.jsx
+++ src/components/signup/signup.jsx
@@ -1,101 +1,27 @@
+import { Button, TextField, Heading } from "@radix-ui/themes";
 import "./signup.css";
 import * as Form from "@radix-ui/react-form";
 const Signup = () => {
   return (
     <div className="d-flex flex-row justify-content-around align-items-center bg-container">
-      <img src="https://img.freepik.com/free-vector/sign-concept-illustration_114360-5267.jpg" />
-      <Form.Root className="FormRoot">
-        <Form.Field className="FormField" name="firstname">
-          <div
-            style={{
-              display: "flex",
-              alignItems: "baseline",
-              justifyContent: "space-between",
-            }}
-          >
-            <Form.Label className="FormLabel">First Name</Form.Label>
-            <Form.Message className="FormMessage" match="valueMissing">
-              Please enter your First Name
-            </Form.Message>
-          </div>
-          <Form.Control asChild>
-            <input className="Input" type="text" required />
-          </Form.Control>
-        </Form.Field>
-        <Form.Field className="FormField" name="lasttname">
-          <div
-            style={{
-              display: "flex",
-              alignItems: "baseline",
-              justifyContent: "space-between",
-            }}
-          >
-            <Form.Label className="FormLabel">Last Name</Form.Label>
-            <Form.Message className="FormMessage" match="valueMissing">
-              Please enter your Last Name
-            </Form.Message>
-          </div>
-          <Form.Control asChild>
-            <input className="Input" type="text" required />
-          </Form.Control>
-        </Form.Field>
-        <Form.Field className="FormField" name="phonenumber">
-          <div
-            style={{
-              display: "flex",
-              alignItems: "baseline",
-              justifyContent: "space-between",
-            }}
-          >
-            <Form.Label className="FormLabel">Phone Number</Form.Label>
-            <Form.Message className="FormMessage" match="valueMissing">
-              Please enter your Mobile Number
-            </Form.Message>
-          </div>
-          <Form.Control asChild>
-            <input className="Input" type="tel" required />
-          </Form.Control>
-        </Form.Field>
-        <Form.Field className="FormField" name="email">
-          <div
-            style={{
-              display: "flex",
-              alignItems: "baseline",
-              justifyContent: "space-between",
-            }}
-          >
-            <Form.Label className="FormLabel">Email</Form.Label>
-            <Form.Message className="FormMessage" match="valueMissing">
-              Please enter your email
-            </Form.Message>
-          </div>
-          <Form.Control asChild>
-            <input className="Input" type="email" required />
-          </Form.Control>
-        </Form.Field>
-        <Form.Field className="FormField" name="password">
-          <div
-            style={{
-              display: "flex",
-              alignItems: "baseline",
-              justifyContent: "space-between",
-            }}
-          >
-            <Form.Label className="FormLabel">Password</Form.Label>
-            <Form.Message className="FormMessage" match="valueMissing">
-              Enter the password
-            </Form.Message>
-          </div>
-          <Form.Control asChild>
-            <input className="Input" type="password" required />
-          </Form.Control>
-        </Form.Field>
-        <Form.Submit asChild>
-          <button className="Button" style={{ marginTop: 10 }}>
-            Login
-          </button>
-        </Form.Submit>
-      </Form.Root>
+      <img src="https://img.freepik.com/premium-vector/illustration-vector-graphic-cartoon-character-online-registration_516790-1805.jpg" />
+      <form className="FormRoot">
+       
+        <Heading className="text-center"> SignUp Form</Heading>
+        <TextField.Root
+          placeholder="First name"
+          className="m-4"
+        ></TextField.Root>
+        <TextField.Root
+          placeholder="Last name"
+          className="m-4"
+        ></TextField.Root>
+        <TextField.Root placeholder="Email" className="m-4"></TextField.Root>
+        <TextField.Root placeholder="Password" className="m-4"></TextField.Root>
+        <div className="text-center">
+          <Button>Sign Up</Button>
+        </div>
+      </form> 
     </div>
   );
 };
