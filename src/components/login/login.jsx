015b9e68bea3c22b4f0fd5c67a2522f19b3db5d4
deekondarushikesh2299 <deekondarushikesh2299@gmail.com>
2024-12-04 20:55:30 +0530

    login signup page trakcing

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,16 +1,47 @@
 import { Button, Heading, TextField } from "@radix-ui/themes";
 import "./login.css";
+import { useState } from "react";
+
 
 const Login = () => {
+  const [formData, setFormData] = useState({
+     email: " ",
+     password: " ",
+    });
+    const onHandleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setFormData({ ... formData, [name]: value });
+    };
+    const submitHandler = (e) =>{
+      e.preventDefault();
+      console.log(formData,"form data sucessfully submited");
+    }
+
+  
+
   return (
     <div className="d-flex flex-row justify-content-around align-items-center bg-container">
       <img src="https://img.freepik.com/free-vector/tablet-login-concept-illustration_114360-7963.jpg?semt=ais_hybrid" />
-      <form className="FormRoot">
+      <form className="FormRoot"  onSubmit={submitHandler}>
         <Heading className="text-center">Login Form</Heading>
-        <TextField.Root  required placeholder="Email" className="m-4"></TextField.Root>
-        <TextField.Root   required  placeholder="Password" className="m-4"></TextField.Root>
+        <TextField.Root
+          required
+          placeholder="Email"
+          className="m-4"
+          onChange={onHandleChange}
+          name="email"
+        ></TextField.Root>
+        <TextField.Root
+          required
+          placeholder="Password"
+          className="m-4"
+          onChange={onHandleChange}
+          name="password"
+          type="password"
+        ></TextField.Root>
         <div className="text-center">
-          <Button>Login</Button>
+          <Button type="submit">Login</Button>
         </div>
       </form>
     </div>
--- src/components/signup/signup.jsx
+++ src/components/signup/signup.jsx
@@ -1,27 +1,59 @@
 import { Button, TextField, Heading } from "@radix-ui/themes";
 import "./signup.css";
-import * as Form from "@radix-ui/react-form";
+import { useState } from "react";
+
 const Signup = () => {
+  const [formData, setFormData] = useState({
+    email: " ",
+    password: " ",
+    firstName: " ",
+    lastName: " ",
+  });
+  const onHandleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setFormData({ ...formData, [name]: value });
+  };
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(formData, "form data sucessfully submited");
+  };
+
   return (
     <div className="d-flex flex-row justify-content-around align-items-center bg-container">
       <img src="https://img.freepik.com/premium-vector/illustration-vector-graphic-cartoon-character-online-registration_516790-1805.jpg" />
-      <form className="FormRoot">
-       
+      <form className="FormRoot" onSubmit={submitHandler}>
         <Heading className="text-center"> SignUp Form</Heading>
         <TextField.Root
           placeholder="First name"
           className="m-4"
+          name="firstName"
+          onChange={onHandleChange}
         ></TextField.Root>
         <TextField.Root
           placeholder="Last name"
           className="m-4"
+          name="lastName"
+          onChange={onHandleChange}
         ></TextField.Root>
-        <TextField.Root placeholder="Email" className="m-4"></TextField.Root>
-        <TextField.Root placeholder="Password" className="m-4"></TextField.Root>
+        <TextField.Root
+          placeholder="Email"
+          className="m-4"
+          name="email"
+          // type="email"
+          onChange={onHandleChange}
+        ></TextField.Root>
+        <TextField.Root
+          placeholder="Password"
+          className="m-4"
+          name="password"
+          type="password"
+          onChange={onHandleChange}
+        ></TextField.Root>
         <div className="text-center">
-          <Button>Sign Up</Button>
+          <Button type="submit">Sign Up</Button>
         </div>
-      </form> 
+      </form>
     </div>
   );
 };
